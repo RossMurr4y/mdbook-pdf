7e14e378e9b82a375585a7df354f243fc4e3fe61
Ross Murray <rossmurray@protonmail.com>
2021-08-01 06:40:51 +0000

    feature: expose pandocs pdf-engine flag as config

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::io;
-use mdbook::{BookItem, Config, config, utils};
+use mdbook::{BookItem};
 use mdbook::renderer::RenderContext;
-use mdbook::book::Chapter;
-use pandoc::DocumentClass::Book;
-use pandoc::Pandoc;
-use toml::{self, Value};
+use std::path::PathBuf;
+use pandoc::{Pandoc, PandocOption};
 
 extern crate serde;
 #[macro_use]
@@ -13,7 +11,59 @@ extern crate serde_derive;
 /// The configuration object for the PDF backend, as represented in `book.toml`.
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct PdfConfig {
-    pub pdf_name: Option<String>,
+    pub output_name: Option<String>,
+    pub pandoc: Option<PandocConfig>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PandocConfig {
+
+    /// The PDF Engine to pass to Pandoc.
+    /// -–pdf-engine=[engine]
+    pub engine: Option<PdfEngineType>,
+}
+
+impl<'a> PandocConfig {
+    pub fn set_engine(&self, pandoc: &'a mut Pandoc) -> &'a Pandoc {
+        let engine_config = self.engine.as_ref();
+        let engine_path: PathBuf = match engine_config.unwrap_or_default() {
+            PdfEngineType::Context => PathBuf::from("context".to_string()),
+            PdfEngineType::Lualatex => PathBuf::from("lualatex".to_string()),
+            PdfEngineType::Pdflatex => PathBuf::from("pdflatex".to_string()),
+            PdfEngineType::Pdfroff => PathBuf::from("pdfroff".to_string()),
+            PdfEngineType::Prince => PathBuf::from("prince".to_string()),
+            PdfEngineType::Weasyprint => PathBuf::from("weasyprint".to_string()),
+            PdfEngineType::Wkhtmltopdf => PathBuf::from("wkhtmltopdf".to_string()),
+            PdfEngineType::Xelatex => PathBuf::from("xelatex".to_string()),
+        };
+        pandoc.add_option(PandocOption::PdfEngine(engine_path))
+    }
+}
+
+impl Default for &PdfEngineType {
+    fn default() -> Self {
+        &PdfEngineType::Xelatex
+    }
+}
+
+impl Default for PandocConfig {
+    fn default() -> Self {
+        Self {
+            engine: Some(PdfEngineType::Xelatex)
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PdfEngineType {
+    Wkhtmltopdf,
+    Weasyprint,
+    Prince,
+    Pdflatex,
+    Lualatex,
+    Xelatex,
+    Pdfroff,
+    Context
 }
 
 impl PdfConfig {
@@ -37,17 +87,24 @@ impl PdfConfig {
         let input_ext = Vec::new();
         let output_ext = Vec::new();
 
-        let mut pandoc: Pandoc = Pandoc::new();
+        let mut pandoc= Pandoc::new();
+        let pandoc_config = self.pandoc.unwrap_or_default();
+        pandoc_config.set_engine(&mut pandoc);
+
         pandoc.set_input_format(pandoc::InputFormat::Commonmark, input_ext);
-        pandoc.set_output_format(pandoc::OutputFormat::Pdf, output_ext);
+        pandoc.set_output_format(pandoc::OutputFormat::Latex, output_ext);
+
+        pandoc.set_variable("monofont", "Liberation Serif");
+        pandoc.set_show_cmdline(true);
+        //pandoc.set_doc_class(pandoc::DocumentClass::Report);
 
         // initialize the content
         let mut content = String::new();
 
         // set output filename
-        let mut filename: std::path::PathBuf = std::path::PathBuf::new();
+        let mut filename: PathBuf = PathBuf::new();
 
-        match self.pdf_name {
+        match self.output_name {
             Some(name) => {
                 filename.push(name);
             }
@@ -77,7 +134,7 @@ impl PdfConfig {
 fn main() {
     let mut stdin = io::stdin();
     let ctx = RenderContext::from_json(&mut stdin).unwrap();
-    let cfg: Option<PdfConfig> = PdfConfig::from_context(&ctx);
+    let cfg = PdfConfig::from_context(&ctx);
     let pandoc = cfg.unwrap().evaluate_opts(ctx);
     pandoc.execute().unwrap();
 }
